0b7180f0b4147743d55c5d09a2d42fbe74893cac
Nikita <Libino123@yandex.ru>
2023-01-12 01:00:23 +0600

    Save

--- Magic/Program.cs
+++ Magic/Program.cs
@@ -6,20 +6,20 @@ int xa = 40, ya = 1,
     xb = 1, yb = 30,
     xc = 80, yc = 30;
 
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
+//Console.SetCursorPosition(xa, ya);
+//Console.WriteLine("+");
 
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
+//Console.SetCursorPosition(xb, yb);
+//Console.WriteLine("+");
 
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+//Console.SetCursorPosition(xc, yc);
+//Console.WriteLine("+");
 
 int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 10) ;
+while (count < 10);
 {
     int what = new Random().Next(0, 3); // 0 1 2
     if (what == 0)
@@ -39,8 +39,8 @@ while (count < 10) ;
         x = (x + xc) / 2;
         y = (y + yc) / 2;
 
-        Console.SetCursorPosition(x, y);
-        Console.WriteLine("+");
-        count++;
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("*");
+    count = count + 1;
     }
 }
